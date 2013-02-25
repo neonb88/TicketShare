449bcd30e34d72ee6e68584fe6289a3c8f1b373a
Kevin Chen <chenkev@umich.edu>
2013-02-25 15:36:13 -0500

    Commit from eclipse

---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+ 
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.Button01);
+        button.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+           }
+         });        
+    }
+}
